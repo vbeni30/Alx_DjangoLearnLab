93ad97daf256b008cf70b51e197ae4ffff4a3b3f
vbeni30 <abenidemiss300@gmail.com>
2026-01-21 11:25:55 +0300

    refactor

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from . import views
-from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import views as auth_views # Use this alias
 
 urlpatterns = [
-    # Book and Library views
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
     
-    # Authentication URLs
-    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    # Use auth_views for login and logout
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+    
+    # Use the function-based view for registration
     path('register/', views.register, name='register'),
 ]

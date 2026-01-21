598f4ca3ed0c0dd8f711dad4181dacf2fafd54e2
vbeni30 <abenidemiss300@gmail.com>
2026-01-21 11:21:30 +0300

    add user auth: login, logout, redistration

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -18,3 +18,18 @@ class LibraryDetailView(DetailView):
 
 def home_view(request):
     return render(request, 'relationship_app/home.html')
+
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('list_books') # Redirect to a page after registration
+    else:
+        form = UserCreationForm()
+    return render(request, 'relationship_app/register.html', {'form': form})

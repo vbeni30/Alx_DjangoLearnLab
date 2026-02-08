c294f2117899717bf06d04886a00864801347e3c
vbeni30 <abenidemiss300@gmail.com>
2026-02-08 23:38:11 +0300

    Implement CRUD using ModelViewSet and DefaultRouter

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,15 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+# Create router
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Existing ListAPIView endpoint
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Router endpoints (CRUD)
+    path('', include(router.urls)),
 ]

c5ec4e2e6b25e83d58f6b88fe16759807f9b7425
vbeni30 <abenidemiss300@gmail.com>
2026-02-19 02:37:42 +0300

    taks one

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import logout
+
+from .forms import RegisterForm, UpdateUserForm
 
 
-def home(request):
-    return render(request, 'blog/base.html')
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            messages.success(request, "Registration successful.")
+            return redirect('profile')
+    else:
+        form = RegisterForm()
+
+    return render(request, 'blog/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+
+    return render(request, 'blog/login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+
+@login_required
+def profile_view(request):
+    if request.method == 'POST':
+        form = UpdateUserForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Profile updated successfully.")
+            return redirect('profile')
+    else:
+        form = UpdateUserForm(instance=request.user)
+
+    return render(request, 'blog/profile.html', {'form': form})

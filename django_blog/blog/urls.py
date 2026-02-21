face68d2a0f9aa174766be8085f62d2a2c6a0d53
vbeni30 <abenidemiss300@gmail.com>
2026-02-21 11:54:07 +0300

    comment

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
 
     # REQUIRED logical comment URL
-    path('posts/<int:post_id>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
 
     path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
